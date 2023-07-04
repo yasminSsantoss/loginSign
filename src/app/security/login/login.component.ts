c2caa86ba007bb415a0626262555f3d489df697d
yasminSsantoss <138533697+yasminSsantoss@users.noreply.github.com>
2023-07-04 20:21:06 -0300

    Update login.component.ts

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  handleSubmit(event: Event) {
+    event.preventDefault();
 
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    const senhaInput = document.getElementById('senha') as HTMLInputElement;
+
+    const formData: FormInput = {
+      email: emailInput.value,
+      senha: senhaInput.value,
+    };
+
+    //formdata 
+
+    (document.getElementById('loginForm') as HTMLFormElement).reset();
+  }
+}
+
+interface FormInput {
+  email: string;
+  senha: string;
 }
